6ab9a645d8071e148c37280da079202977dac3fd
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-28 23:42:28 +0300

    add SceneWindow

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 #include "Camera.h"
 #include "ScrollBar.h"
 
+
+
+// const ButtonTexturePath scrollBarTopBtnPath    = {"images/scrollBar/topButton/unpressed.png", "images/scrollBar/topButton/pressed.png"};
+// const ButtonTexturePath scrollBarBottomBtnPath = {"images/scrollBar/bottomButton/unpressed.png", "images/scrollBar/bottomButton/pressed.png"};
+// const ButtonTexturePath scrollThumbBtnPath     = {"images/scrollBar/thumbButton/unpressed.png", "images/scrollBar/thumbButton/pressed.png"};
+
+
 const char FONT_PATH[] = "fonts/Roboto/RobotoFont.ttf";
 const std::pair<int, int> MAIN_WINDOW_SIZE = {1000, 600};
 const int APP_BORDER_SIZE = 20;
@@ -164,28 +171,6 @@ public:
 
 
 
-// class ObjectsListWidget : public Widget {
-
-// };
-
-
-// class OjectsListWindow : public Window {
-//     ObjectsListWidget *objectList = nullptr;
-//     ScrollBar *scalbar = nullptr;
-// };
-
-
-// class SceneWindow : public Window {
-//     CameraWindow *cameraWindow = nullptr;
-    
-//     SceneWindow(int w, int h, Widget *parent=nullptr): Window(w, h, this) { 
-//         cameraWindow = new CameraWindow(SCREEN_RESOLUTION.first, SCREEN_RESOLUTION.second, this);
-//         addWidget(0, 0, cameraWindow);
-//     }
-// };
-
-
-
 class TextWidget : public Widget {
 protected:
     std::string text_;
@@ -315,20 +300,69 @@ public:
 
 
 
+class ObjectsListWidget : public Widget {
+
+};
 
 
-double measureRenderTime(SceneManager &sceneManager, Camera &camera, const std::size_t MEASURE_COUNT=1) {
-    double duration = 0;
-    for (std::size_t i = 0; i < MEASURE_COUNT; i++) {
-        auto start = std::chrono::high_resolution_clock::now();
-        sceneManager.render(camera);
-        auto end = std::chrono::high_resolution_clock::now();  
-     
-        duration += std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+class OjectsListWindow : public Window {
+    ObjectsListWidget *objectList = nullptr;
+    // ScrollBar *scalbar = nullptr;
+};
+
+
+class SceneWindow : public Window {
+    SceneManager sceneManager_;
+    Camera camera_;
+    CameraWindow *cameraWindow_ = nullptr;
+
+public:
+    SceneWindow(int w, int h, Widget *parent=nullptr): 
+        Window(w, h, parent), 
+        sceneManager_(), camera_(/*center*/{0, -6, 1}, /*direction*/{0, 3, 0}, RENDER_SCREEN_RESOLUTION) 
+    { 
+        camera_.setSamplesPerScatter(1);
+        camera_.setSamplesPerPixel(1);
+        camera_.disableLDirect();
+        camera_.setMaxRayDepth(5);
+        camera_.setThreadPixelbunchSize(100);
+        // camera.disableParallelRender();
+
+        cameraWindow_ = new CameraWindow(w, h, this);
+
+        cameraWindow_->setCamera(&camera_);
+        addWidget(0, 0, cameraWindow_);
     }
 
-    return duration / MEASURE_COUNT;
-}
+    void addObject(gm::IPoint3 position, Primitives *object) { sceneManager_.addObject(position, object); }
+    void addLight(gm::IPoint3 position, Light *light) { sceneManager_.addLight(position, light); }
+    void renderSelfAction(SDL_Renderer* renderer) override { 
+        camera_.render(sceneManager_);
+        cameraWindow_->render(renderer);
+    }
+
+
+    double measureRenderTime(const std::size_t MEASURE_COUNT=1) {
+        double duration = 0;
+        for (std::size_t i = 0; i < MEASURE_COUNT; i++) {
+            auto start = std::chrono::high_resolution_clock::now();
+            sceneManager_.render(camera_);
+            auto end = std::chrono::high_resolution_clock::now();  
+        
+            duration += std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        }
+
+        return duration / MEASURE_COUNT;
+    }
+
+
+
+    const Camera &camera() const { return camera_; }
+    const SceneManager &sceneManager() const { return sceneManager_; }
+
+
+};
+
 
 void textReceived(const std::string &str) {
     std::cout << "textReceived : `" << str << "`" << "\n";
@@ -339,23 +373,12 @@ int main() {
     Container *mainWindow = new Container(MAIN_WINDOW_SIZE.first - 2 * APP_BORDER_SIZE, MAIN_WINDOW_SIZE.second - 2 * APP_BORDER_SIZE);
     application.setMainWidget(APP_BORDER_SIZE, APP_BORDER_SIZE, mainWindow);
 
-    SceneManager sceneManager;
+    SceneWindow *sceneWindow = new SceneWindow(RENDER_SCREEN_RESOLUTION.first, RENDER_SCREEN_RESOLUTION.second, mainWindow);
+    mainWindow->addWidget(0, 0, sceneWindow);
 
-    CameraWindow *cameraWindow = new CameraWindow(RENDER_SCREEN_RESOLUTION.first, RENDER_SCREEN_RESOLUTION.second, mainWindow);
-    mainWindow->addWidget(0, 0, cameraWindow);
 
     TextInputWidget *textField = new TextInputWidget(300, 100, "23423", BLACK_SDL_COLOR, 32, FONT_PATH, textReceived, mainWindow);
     mainWindow->addWidget(650, 0, textField);
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -373,25 +396,9 @@ int main() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // SphereObject *light = new SphereObject(1, lightSrcMaterial, &sceneManager);
 
-    SphereObject *sun = new SphereObject(1, sunMaterial, &sceneManager);
+    SphereObject *sun = new SphereObject(1, sunMaterial, &sceneWindow->sceneManager());
 
     Light *light = new Light
     (
@@ -401,45 +408,33 @@ int main() {
         /* viewLightPow      */  15.0
     );
 
-    SphereObject    *midSphere = new SphereObject(1, midSphereMaterial, &sceneManager);
-    SphereObject    *rightSphere = new SphereObject(1, rightSphereMaterial, &sceneManager);
+    SphereObject    *midSphere = new SphereObject(1, midSphereMaterial, &sceneWindow->sceneManager());
+    SphereObject    *rightSphere = new SphereObject(1, rightSphereMaterial, &sceneWindow->sceneManager());
   
-    PlaneObject     *ground = new PlaneObject({0, 0, 0}, {0, 0, 1}, groundMaterial, &sceneManager);
+    PlaneObject     *ground = new PlaneObject({0, 0, 0}, {0, 0, 1}, groundMaterial, &sceneWindow->sceneManager());
 
 
 
-    SphereObject    *glassSphere = new SphereObject(1, glassMaterial, &sceneManager);
+    SphereObject    *glassSphere = new SphereObject(1, glassMaterial, &sceneWindow->sceneManager());
 
     
-    sceneManager.addObject({0, 0, -100}, ground);
-    sceneManager.addObject({0, 0, 1}, glassSphere);
+    sceneWindow->addObject({0, 0, -100}, ground);
+    sceneWindow->addObject({0, 0, 1}, glassSphere);
     // // sceneManager.addObject({-2, 0, 1}, leftBubbleSphere);
-    sceneManager.addObject({0, 4, 3}, midSphere);
-    sceneManager.addObject({2, 0, 1}, rightSphere);
+    sceneWindow->addObject({0, 4, 3}, midSphere);
+    sceneWindow->addObject({2, 0, 1}, rightSphere);
 
-    sceneManager.addLight({0, 0, 10}, light);
-    sceneManager.addObject({-2, 0, 4}, sun);
+    sceneWindow->addLight({0, 0, 10}, light);
+    sceneWindow->addObject({-2, 0, 4}, sun);
 
-    Camera camera(/*center*/{0, -6, 1}, /*direction*/{0, 3, 0}, RENDER_SCREEN_RESOLUTION);
-    camera.setSamplesPerScatter(1);
-    camera.setSamplesPerPixel(1);
-    camera.disableLDirect();
-    camera.setMaxRayDepth(5);
-    camera.setThreadPixelbunchSize(100);
-    // camera.disableParallelRender();
-
-
-
-
-    cameraWindow->setCamera(&camera);
-
+ 
 
     // // !!!!! WARNING  
 
-    std::cout << "renderTime : " << measureRenderTime(sceneManager, camera) << '\n';
+    std::cout << "renderTime : " << sceneWindow->measureRenderTime() << "\n"; 
 
         
-    application.addUserEvent([&sceneManager, &camera](int deltaMS) { sceneManager.render(camera);});
+    // application.addUserEvent([sceneWindow](int deltaMS) { sceneWindow->render();});
     application.run();
 
 }
