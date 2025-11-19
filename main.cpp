dea583f25462e14248a39e976ff015ec1b6af7f5
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-11-19 15:33:56 +0300

    add working demo

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include "misc/dr4_ifc.hpp"
 
 const char PLAGIN_PATH[] = "external/libAIPlugin.so";
-typedef dr4::DR4Backend* (*DR4BackendFunction)();
+const char FONT_PATH[] = "assets/RobotoFont.ttf";
 
+typedef dr4::DR4Backend* (*DR4BackendFunction)();
 
 int main() {
     void *backendLib = dlopen(PLAGIN_PATH, RTLD_LAZY);
@@ -13,6 +14,64 @@ int main() {
     dr4::DR4Backend *backend = func(); assert(backend);
     dr4::Window *window = backend->CreateWindow(); assert(window);
 
+    assert(window);
 
-    std::cout << "Hello, World!\n";
+    window->Open();
+    window->SetSize({800, 600});
+
+    dr4::Rectangle *rect = window->CreateRectangle();
+    rect->SetBorderColor({0, 255, 0, 200});
+    rect->SetFillColor({200, 0, 0, 100});
+    rect->SetPos({30, 30});
+    rect->SetSize({100, 200});
+    rect->SetBorderThickness(10);
+
+    dr4::Circle *circle = window->CreateCircle();
+    circle->SetBorderColor({0, 0, 255, 255});
+    circle->SetBorderThickness(10);
+    circle->SetCenter({100, 100});
+    circle->SetFillColor({0, 0, 0, 0});
+    circle->SetRadius(30);
+
+
+    dr4::Text *text = window->CreateText();
+    dr4::Font *font = window->CreateFont();
+    font->LoadFromFile(FONT_PATH);
+    text->SetFont(font);
+    text->SetColor({0, 255, 0, 150});
+    text->SetVAlign(dr4::Text::VAlign::MIDDLE);
+    text->SetFontSize(30);
+    text->SetText("Ia obyzatelno vizhivu!");
+
+    dr4::Texture *texture = window->CreateTexture();
+    texture->SetSize({500, 500});
+    texture->SetPos({50, 50});
+    texture->Clear({255, 255, 255, 255});
+    texture->SetZero({0, 0});
+
+    
+    rect->DrawOn(*texture);
+    circle->DrawOn(*texture);
+
+    text->SetPos(100, 100);
+    text->DrawOn(*texture);
+    
+    while (true) {
+        window->Clear({0, 0, 0, 255});
+        window->Draw(*texture);
+        window->Display();
+
+        std::optional<dr4::Event> event = window->PollEvent();
+        if (event.has_value() && event.value().type == dr4::Event::Type::QUIT) {
+            break;
+        }
+    }
+   
+    delete window;
+    delete backend;
+    delete circle;
+    delete texture;
+    delete rect;
+    delete text;
+    delete font;
 }
