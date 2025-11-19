81546ca7065e75563af47631d6a78e1d513f7a33
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-11-19 15:59:37 +0300

    add drawing widget

--- main.cpp
+++ main.cpp
@@ -2,24 +2,16 @@
 #include <dlfcn.h>
 #include <cassert>
 #include "misc/dr4_ifc.hpp"
+#include "hui/widget.hpp"
+#include "hui/state.hpp"
 
 const char PLAGIN_PATH[] = "external/libAIPlugin.so";
 const char FONT_PATH[] = "assets/RobotoFont.ttf";
 
 typedef dr4::DR4Backend* (*DR4BackendFunction)();
 
-int main() {
-    void *backendLib = dlopen(PLAGIN_PATH, RTLD_LAZY);
-    DR4BackendFunction func = (DR4BackendFunction) dlsym(backendLib, dr4::DR4BackendFunctionName); assert(func);
-    dr4::DR4Backend *backend = func(); assert(backend);
-    dr4::Window *window = backend->CreateWindow(); assert(window);
-
-    assert(window);
-
-    window->Open();
-    window->SetSize({800, 600});
-
-    dr4::Rectangle *rect = window->CreateRectangle();
+void test(dr4::Window *window) {
+ dr4::Rectangle *rect = window->CreateRectangle();
     rect->SetBorderColor({0, 255, 0, 200});
     rect->SetFillColor({200, 0, 0, 100});
     rect->SetPos({30, 30});
@@ -67,11 +59,58 @@ int main() {
         }
     }
    
-    delete window;
-    delete backend;
+    
+
     delete circle;
     delete texture;
     delete rect;
     delete text;
     delete font;
+}
+
+int main() {
+    void *backendLib = dlopen(PLAGIN_PATH, RTLD_LAZY);
+    DR4BackendFunction func = (DR4BackendFunction) dlsym(backendLib, dr4::DR4BackendFunctionName); assert(func);
+    dr4::DR4Backend *backend = func(); assert(backend);
+    dr4::Window *window = backend->CreateWindow(); assert(window);
+    assert(window);
+
+    window->Open();
+    window->SetSize({800, 600});
+
+
+    hui::State state(window);
+    hui::Widget *wgt = new hui::Widget(&state);
+    assert(wgt);
+
+    dr4::Texture *texture = window->CreateTexture();
+    assert(texture);
+
+    texture->SetSize(600, 400);
+    texture->SetPos(100, 100);
+    texture->Clear({255, 255, 255, 255});
+
+    wgt->SetPos(30, 30);
+    wgt->SetSize(100, 100);
+    wgt->DrawOn(*texture);
+    
+    
+    while (true) {
+        window->Clear({0, 0, 0, 255});
+        window->Draw(*texture);
+        window->Display();
+
+        std::optional<dr4::Event> event = window->PollEvent();
+        if (event.has_value() && event.value().type == dr4::Event::Type::QUIT) {
+            break;
+        }
+    }
+
+
+
+
+    delete window;
+    delete backend;
+    delete texture;
+    delete wgt;
 }
